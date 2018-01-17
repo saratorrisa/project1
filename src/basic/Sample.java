8932cc746ea49552afa570feca66cc90b5968e23
Sarath-PC <Sarath-PC@192.168.29.1>
2018-01-17 17:35:34 +0530

    first commit

---
+++ src/basic/Sample.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Sample {
+
+	public static void main(String[] args)
+	{
+		System.out.println("hai");
+
+	}
+
+}
---
+++ src/basic/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Sarath-PC
+ *
+ */
+package basic;
